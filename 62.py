4fbea175d51916a3d296adfbbe83851e4a653212
Tao Dai <tao.dai36@gmail.com>
2017-11-02 02:16:44 -0400

    62

---
+++ 62.py
@@ -0,0 +1,27 @@
+# Unique Paths
+
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+#
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+#
+# How many possible unique paths are there?
+
+# interesting dp
+# p(x, y) = p(x-1, y) + p(x, y-1)
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0 for i in range(n)] for j in range(m)]
+        for i in range(n):
+            dp[0][i] = 1
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
